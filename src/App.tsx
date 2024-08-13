424c68314f2330ac1eeedb1cdcaf512b3da3b48e
Andrés Felipe Sanguino Sanguino <andresfelipesasa@ufps.edu.co>
2024-08-13 14:55:22 -0500

    Footer inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from './components/navbar.tsx'
+import Footer from './components/footer.tsx'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Layout } from 'antd';
+import { Col, Row } from 'antd';
+import { Form, Input, List, Space } from 'antd';
+import { FacebookOutlined, InstagramOutlined, LinkedinOutlined, WhatsAppOutlined } from '@ant-design/icons';
+
+const { Footer } = Layout;
+
+type FieldType = {
+    mail?: string;
+};
+
+const Footer2: React.FC = () => {
+    return (
+        <Layout>
+            <Footer style={{ alignItems: 'center', alignContent: 'center', backgroundColor: '#EDC400' }}>
+                <Row>
+                    <Col span={10}>
+                        <h3>OFERTAS Y PROMOCIONES</h3>
+                        <h1>No te pierdas nuestras ofertas!</h1>
+                        <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} style={{ maxWidth: 600 }} initialValues={{ remember: true }} autoComplete="off">
+                            <Form.Item<FieldType> name="mail" rules={[{ required: true, message: 'Ingresa un correo electronico.' }]}>
+                                <Input />
+                            </Form.Item>
+                        </Form>
+                        <Row style={{ alignItems: 'center' }} justify="space-between">
+                            <Col>
+                                <img src="../Logotipo-Macropay.svg" alt="MacroPay" />
+                            </Col>
+                            <Col>
+                                <List>
+                                    <List.Item>Envios y devoluciones</List.Item>
+                                    <List.Item>Preguntas frecuentes</List.Item>
+                                </List>
+                            </Col>
+                            <Col>
+                                <List>
+                                    <List.Item>Aviso de privacidad</List.Item>
+                                    <List.Item>Términos y condiciones</List.Item>
+                                </List>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col span={4} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <img src="../Happy_face.svg" alt="MacroPay" style={{ width: '60%' }} />
+                    </Col>
+                    <Col span={8} offset={2} style={{alignItems: 'center', justifyContent: 'center', alignContent: 'center'}}>
+                        <h1>Conversemos!</h1>
+                        <p>Loren ipsum dolor sit amet</p>
+                        <Space size="large">
+                            <a href="#"><FacebookOutlined style={{ fontSize: '35px', color: 'black'}} /></a>
+                            <a href="#"><InstagramOutlined style={{ fontSize: '35px', color: 'black' }} /></a>
+                            <a href="#"><LinkedinOutlined style={{ fontSize: '35px', color: 'black' }} /></a>
+                            <a href="#"><WhatsAppOutlined style={{ fontSize: '35px', color: 'black' }} /></a>
+                        </Space>
+                    </Col>
+                </Row>
+            </Footer>
+        </Layout>
+    );
+};
+
+export default Footer2;
